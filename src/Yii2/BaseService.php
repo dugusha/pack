8d0d69ccb8c69d6ed8c1e196871babcea9cb1db4
gaoruishen <gaoruishen@gaoruishen.local>
2020-03-25 16:55:44 +0800

    fix bug

---
+++ src/Yii2/BaseService.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gaoruishen
+ * Date: 2020/3/25
+ * Time: 4:54 PM
+ */
+
+namespace GRS\Yii2;
+
+use Yii;
+use yii\base\Component;
+
+class BaseService extends Component
+{
+    /**
+     * 单例
+     * @return static
+     */
+    public static function  getInstance() {
+        $obj = Yii::createObject(static::className());
+        if(!Yii::$container->hasSingleton(static::className())) {
+            Yii::$container->setSingleton(static::className(),$obj);
+        }
+        return $obj;
+    }
+}
