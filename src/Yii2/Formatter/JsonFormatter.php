62aa6ba57a3f183dd1a3569ca10fffadaec0c0c2
gaoruishen <gaoruishen@gaoruishen.local>
2020-03-27 16:30:28 +0800

    fix bug

--- src/Yii2/Formatter/JsonFormatter.php
+++ src/Yii2/Formatter/JsonFormatter.php
@@ -18,7 +18,8 @@ class JsonFormatter extends JsonResponseFormatter {
         if(!isset($response->data['ret'])) {
             if(isset($response->data['__exception__'])) {
                 unset($response->data['__exception__']);
-                $response->data = ['code'=>-1,'msg'=>$response->data,'data'=>(object)[]];
+                $code = empty($response->data["code"])?-1:$response->data["code"];
+                $response->data = ['code'=>$code,'msg'=>$response->data["msg"],'data'=>$response->data];
             } else {
                 $response->data = ['code'=>0,'msg'=>'','data'=>$response->data];
             }
