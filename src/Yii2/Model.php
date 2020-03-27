6596c8cfc57354b9baaa93f2126c044e442d6c95
gaoruishen <gaoruishen@gaoruishen.local>
2020-03-27 17:31:26 +0800

    fix bug

--- src/Yii2/Model.php
+++ src/Yii2/Model.php
@@ -18,21 +18,17 @@ class Model extends ActiveRecord{
     }
 
     /**
-     * 插入新数据
-     * 插入失败返回null
-     * 插入成功返回成功的model
-     * @param array $attributes
-     * @return null|static
+     * 增强yii2的find方法，增加分页paginate
+     * 例子：
+     * Model::find()->where($params)->paginate($page,$pageSize)
+     * 如果需要切换db
+     * Model::find()->where($params)->paginate($page,$pageSize,$db)
+     * @return Page
      */
-    public static function add(array $attributes) {
-        if(empty($attributes)) return null;
-        $model = new static;
-        foreach($attributes as $column=>$value) {
-            $model->hasAttribute($column) && $model[$column] = $value;
-        }
-        if($model->save()) return $model;
-        return null;
+    public static function find() {
+        return Yii::createObject(Page::className(), [get_called_class()]);
     }
+
     /**
      * 为参数增加表前缀
      *
@@ -54,6 +50,23 @@ class Model extends ActiveRecord{
     }
 
     /**
+     * 插入新数据
+     * 插入失败返回null
+     * 插入成功返回成功的model
+     * @param array $attributes
+     * @return null|static
+     */
+    public static function add(array $attributes) {
+        if(empty($attributes)) return null;
+        $model = new static;
+        foreach($attributes as $column=>$value) {
+            $model->hasAttribute($column) && $model[$column] = $value;
+        }
+        if($model->save()) return $model;
+        return null;
+    }
+
+    /**
      * 增强insert方法,自动插入c_t,u_t
      */
     public function insert($runValidation = true, $attributes = null) {
@@ -71,15 +84,38 @@ class Model extends ActiveRecord{
     }
 
     /**
-     * 增强yii2的find方法，增加分页paginate
-     * 例子：
-     * Model::find()->where($params)->paginate($page,$pageSize)
-     * 如果需要切换db
-     * Model::find()->where($params)->paginate($page,$pageSize,$db)
-     * @return Query
+     * 增强yii2 批量插入
+     * @param $columns
+     * @param $values
+     * @return bool|int
+     * @throws \yii\db\Exception
      */
-    public static function find() {
-        return Yii::createObject(Page::className(), [get_called_class()]);
+    public static function batchInsert($columns, $values) {
+        $queryBuilder = static::getDb()->getQueryBuilder();
+        if(empty($values))  return false;
+        $model = new static;
+        $time = time();
+
+        if(!in_array('c_t',$columns) && $model->hasAttribute('c_t')) {
+            $columns[] = 'c_t';
+            foreach($values as &$value) {
+                if(!is_array($value))  continue;
+                $value[] = $time;
+            }
+        }
+
+        if(!in_array('u_t',$columns) && $model->hasAttribute('u_t')) {
+            $columns[] = 'u_t';
+            foreach($values as &$value) {
+                if(!is_array($value)) continue;
+                $value[] = $time;
+            }
+        }
+
+        $sql  = $queryBuilder->batchInsert(static::tableName(),$columns,$values);
+        $command = static::getDb()->createCommand($sql);
+        return $command->execute();
+
     }
 
     /**
@@ -116,41 +152,6 @@ class Model extends ActiveRecord{
             $attributes['u_t'] = time();
         }
         return parent::updateAll($attributes,$condition,$params);
-    }
-
-    /**
-     * 增强yii2 批量插入
-     * @param $columns
-     * @param $values
-     * @return bool|int
-     * @throws \yii\db\Exception
-     */
-    public static function batchInsert($columns, $values) {
-        $queryBuilder = static::getDb()->getQueryBuilder();
-        if(empty($values))  return false;
-        $model = new static;
-        $time = time();
-
-        if(!in_array('c_t',$columns) && $model->hasAttribute('c_t')) {
-            $columns[] = 'c_t';
-            foreach($values as &$value) {
-                if(!is_array($value))  continue;
-                $value[] = $time;
-            }
-        }
-
-        if(!in_array('u_t',$columns) && $model->hasAttribute('u_t')) {
-            $columns[] = 'u_t';
-            foreach($values as &$value) {
-                if(!is_array($value)) continue;
-                $value[] = $time;
-            }
-        }
-
-        $sql  = $queryBuilder->batchInsert(static::tableName(),$columns,$values);
-        $command = static::getDb()->createCommand($sql);
-        return $command->execute();
-
     }
 
     /**
